1c18fb1befba3994dbc23041cf4e13a8a7deb962
Erdem Kapkara <erdemkapkara.uk@gmail.com>
2022-03-08 10:07:14 +0000

    Update

--- Pongie/Assets/Scripts/Managers/GameManager.cs
+++ Pongie/Assets/Scripts/Managers/GameManager.cs
@@ -18,21 +18,104 @@ public class GameManager : MonoBehaviour
     private string empty = " ";
 
     public Button ButtonClick;
-    public BallManager Ballmanager;
-    public GameObject Ball;// Field
+    public Button ButtonPlay;
+    public Button ButtonPlayAgain;
+    public Button Exit;
+    public BallController Ballcontroller;
+    public GameObject Ball;
+    public GameObject Players;
+    public int CountDownTime;
+    public TextMeshProUGUI CountDownText;
+    public TextMeshProUGUI NextLevelText;
+    private int LevelCount=1;
     private int MaxScore { get { return gameSettings.MaxScore; } }
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+    void Awake()
+    {
+        if (_instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }        
+    }
+
     public void FinishGame()
     {
         if (score1 > score2)
         {
+
             txt3.text = win;
+            LevelCount++;
+            ResetScore();
+            NextLevel();
         }
         else
         {
             txt3.text = lost;
+
+
+            Button PlayAgainButton = ButtonPlayAgain.GetComponent<Button>();
+            PlayAgainButton.onClick.AddListener(PlayAgain);
+
+            Button ButtonExit = Exit.GetComponent<Button>();
+            ButtonExit.onClick.AddListener(GameExit);
+            LevelCount = 1;
+            ResetScore();
         }
-        Ball.SetActive(false);
-        Ballmanager.FinishGame();
+     
+    }
+    IEnumerator CountDownToStart()
+    {
+        while (CountDownTime >= 0)
+        {
+            CountDownText.text = CountDownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            CountDownTime--;
+        }
+        CountDownText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        Ball.SetActive(true);
+        Players.SetActive(true);
+    }
+    IEnumerator NextLevelLoading()
+    {
+        CountDownTime = 5;
+        while (CountDownTime >= 0)
+        {
+            NextLevelText.text ="Loading"+" "+"Level"+" "+LevelCount+" "+CountDownTime.ToString()+" "+"...";
+            yield return new WaitForSeconds(1f);
+            CountDownTime--;
+        }
+        yield return new WaitForSeconds(2f);
+        CountDownText.gameObject.SetActive(false);
+    }
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+    public void PlayAgain()
+    {
+        //Ballcontroller.FinishGame();
+    }
+    public void NextLevel()
+    {
+        StartCoroutine(NextLevelLoading());
     }
     public void ResetScore()
     {
@@ -41,8 +124,8 @@ public class GameManager : MonoBehaviour
         txt1.text = score1.ToString();
         txt2.text = score2.ToString();
         txt3.text = empty;
-        Ball.SetActive(true);
-        Ballmanager.ResetScore();  
+        //Ball.SetActive(true);
+        Ballcontroller.ResetScore();  
     }
     public void UpdateScore(int player)
     {
@@ -57,23 +140,26 @@ public class GameManager : MonoBehaviour
             txt1.text = score1.ToString();
         }
     }
+    public void PlayGame()
+    {
+        Ball.SetActive(false);
+        Players.SetActive(false);
+        StartCoroutine(CountDownToStart());
+    }
     void Start()
     {
-        Ball = GameObject.Find("Ball");
-
         Button btn = ButtonClick.GetComponent<Button>();
         btn.onClick.AddListener(ResetScore);
+        
+        Button PlayButton = ButtonPlay.GetComponent<Button>();
+        PlayButton.onClick.AddListener(PlayGame);
     }
-    public void Play()
-    {
-        Ball.SetActive(true);
-        ResetScore();
-    }
+
     void Update()
     {
         if (score1 >= MaxScore || score2 >= MaxScore)
         {
-            FinishGame();
+            FinishGame(); 
         }
     }
 }
